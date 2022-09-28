bcf41e48aa59d608acd1ff0d1a08150c91a2fbcc
sultanshotel <info@sultanshotel.com>
2022-09-28 14:17:51 -0400

    add

---
+++ TODO.md
@@ -0,0 +1,2 @@
+https://caiorcferreira.github.io/post/golang-secure-api-key-middleware/
+https://drstearns.github.io/tutorials/gojson/
--- sil.js
+++ sil.js
@@ -1,6 +1,27 @@
 var s = "bl"
 
-var o = {
-  k: 5,
-  m: "s",
+var o = { k: 5, m: "s" }
+
+function Foo({ props }) {
+  return (
+    <div>
+      <h1>Masallah</h1>
+    </div>
+  )
+}
+
+function Greet(user) {
+  return (
+    <>
+      <Foo
+        p1="1"
+        p2="2"
+      />
+    </>
+  )
+}
+
+// {user ? `Welcome back, ${user.name}!` : "Greetings, traveler! Sign up today!"}
+function greet(user) {
+  return user ? `Welcome back, ${user.name}!` : "Greetings, traveler! Sign up today!"
 }
